79355c447c7bdf6f566f097ca00e79fd37078066
rslangl <fjellape1447@protonmail.com>
2025-07-05 11:36:03 +0200

    refactor: switch to rustaceanvim

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,40 +1,5 @@
-print("[LSP] Loading rust plugin")
-
 return {
-  {
-    "simrat39/rust-tools.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "nvim-lua/plenary.nvim",
-    },
-    ft = { "rust" },
-    config = function()
-      local rt = require("rust-tools")
-
-      rt.setup({
-        server = {
-          cmd = { vim.fn.expand("~/.nix-profile/bin/rust-analyzer") },
-          settings = {
-            ["rust-analyzer"] = {
-              cargo = {
-                allFeatures = true,
-              },
-              checkOnSave = {
-                command = "clippy",
-              },
-              procMacro = {
-                enable = true,
-              },
-            },
-          },
-        },
-        tools = {
-          hover_actions = {
-            auto_focus = true,
-          },
-        },
-      })
-    end,
-  },
+  'mrcjkb/rustaceanvim',
+  version = '^6', -- Recommended
+  lazy = false, -- This plugin is already lazy
 }
-
