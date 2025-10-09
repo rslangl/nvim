aaa00d36b22b760568d196aca828ebcd1b116c3d
rslangl <151566232+rslangl@users.noreply.github.com>
2025-10-09 23:08:52 +0200

    fix: treesitter config path (#6)

--- lua/plugins/syntax_highlighting.lua
+++ lua/plugins/syntax_highlighting.lua
@@ -1,9 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  --- event = { "BufReadPost", "BufNewFile" },
+  event = { "BufReadPost", "BufNewFile" },
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter.config").setup({
       ensure_installed = {
         "lua",
         "nix",
