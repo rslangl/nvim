2bd4df3a23b4bbfc1194c7c43379b5668899d93f
rslangl <151566232+rslangl@users.noreply.github.com>
2025-10-11 23:17:06 +0200

    chore: add LSP integration for Rust (#7)
    Co-authored-by: user <mailman@kek.net>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- Run LSP code action
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action"})
+
+-- Rust-specific debug action
+vim.keymap.set("n", "<leader>dr", function ()
+  vim.cmd.RustLsp("debuggables")
+end, { desc = "Rust debuggables" })
