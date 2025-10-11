684c85739dc56e702e0f36776c3756e085ba6d70
user <mailman@kek.net>
2025-10-11 23:07:53 +0200

    chore: add LSP integration for Rust

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,6 +1,38 @@
 return {
   -- Core completion plugin
-  { "hrsh7th/nvim-cmp", enabled = true, event = "InsertEnter" },
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = true,
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function ()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-e>"] = cmp.mapping.abort(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end
+  },
 
   -- LSP source for nvim-cmp, must load **after** nvim-cmp
   { "hrsh7th/cmp-nvim-lsp", dependencies = "nvim-cmp", event = "InsertEnter" },
